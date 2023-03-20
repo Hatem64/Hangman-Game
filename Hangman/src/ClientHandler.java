192c17cfbd2bdd9173c02f034dd4b7ad8935c31b
AhmedHatem001 <ah.hatem225@gmail.com>
2023-03-21 00:13:24 +0200

    Finished 80% of the single player
    what is left is cleaning up the menu sequences, adding the scores and the attempts with each server msg, and finally doing a hard level for the single player, which will just be change of files and number of tries.

--- Hangman/src/ClientHandler.java
+++ Hangman/src/ClientHandler.java
@@ -9,13 +9,16 @@ public class ClientHandler implements Runnable{
 
     private Socket client;
     private Scanner scanner = null;
-    String clientMsg = "";
-
+//    String clientMsg = "";
+    String functionMsg = "";
     String serverMsg = "";
     DataOutputStream dataOutputStream;
     DataInputStream dataInputStream;
     ImpUserServices impUserServices = new ImpUserServices();
+    SinglePlayer singlePlayer;
     String selected = "";
+    ArrayList<String> clientMsgs = new ArrayList<>();
+
     public ClientHandler(Socket socket) {
         this.client = socket;
         try {
@@ -31,9 +34,24 @@ public class ClientHandler implements Runnable{
     @Override
     public void run() {
         try {
-            while (!clientMsg.equals("-")){
-                ArrayList<String> clientMsgs = new ArrayList<>();
-                serverMsg = "Plz select one of the following! \n 1-login \n 2-register";
+            //Menu Functions will be placed here!
+            functionMsg = registrationAndLoginMenu();
+            if (functionMsg.equals("2")){
+                gameMenu();
+
+            }
+            dataOutputStream.close();
+            scanner.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String registrationAndLoginMenu(){
+        try {
+            while (true){
+                String[] returnedMsg = null;
+                serverMsg = "1,Welcome! Please login to play or create a new user!! \n 1-Login \n 2-Register \n 3-Exit Program";
                 dataOutputStream.writeUTF(serverMsg);
                 dataOutputStream.flush();
                 selected = dataInputStream.readUTF();
@@ -45,9 +63,15 @@ public class ClientHandler implements Runnable{
                             dataOutputStream.writeUTF(serverMsg);
                             dataOutputStream.flush();
                             clientMsgs.add((String) dataInputStream.readUTF());
-                            System.out.println("Client: " + clientMsg);
+//                            System.out.println("Client: " + clientMsg);
                         }
                         serverMsg = impUserServices.login(clientMsgs.get(0), clientMsgs.get(1));
+                        returnedMsg = serverMsg.split(",");
+                        if ( returnedMsg[0].equals("2")){
+                            dataOutputStream.writeUTF(serverMsg);
+                            dataOutputStream.flush();
+                            return "2";
+                        }
                         dataOutputStream.writeUTF(serverMsg);
                         dataOutputStream.flush();
                         break;
@@ -57,17 +81,54 @@ public class ClientHandler implements Runnable{
                             dataOutputStream.writeUTF(serverMsg);
                             dataOutputStream.flush();
                             clientMsgs.add((String) dataInputStream.readUTF());
-                            System.out.println("Client: " + clientMsg);
+//                            System.out.println("Client: " + clientMsg);
                         }
-                        impUserServices.register(clientMsgs.get(0),clientMsgs.get(1),clientMsgs.get(2));
+                        serverMsg = impUserServices.register(clientMsgs.get(0),clientMsgs.get(1),clientMsgs.get(2));
+                        dataOutputStream.writeUTF(serverMsg);
+                        dataOutputStream.flush();
                         break;
+                    case "3":
+                        return "exit";
                 }
 
             }
-            dataInputStream.close();
-            dataOutputStream.close();
-            scanner.close();
-        } catch (IOException e) {
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String gameMenu() {
+        try {
+            while (true){
+                String[] returnedMsg = null;
+                serverMsg = "1,Select one of following options! \n 1-Single Player \n 2-Multiplayer \n 3-Show Score History \n 4-Exit";
+                dataOutputStream.writeUTF(serverMsg);
+                dataOutputStream.flush();
+                selected = dataInputStream.readUTF();
+                System.out.println("Client: " + selected);
+                switch (selected){
+                    case "1":
+                        singlePlayer = new SinglePlayer(clientMsgs, client);
+                        singlePlayer.selectGameDifficulty();
+                        break;
+                    case "2":
+                        for(int i =0; i<3; i++){
+                            serverMsg = "";
+                            dataOutputStream.writeUTF(serverMsg);
+                            dataOutputStream.flush();
+                            clientMsgs.add((String) dataInputStream.readUTF());
+//                            System.out.println("Client: " + clientMsg);
+                        }
+                        serverMsg = impUserServices.register(clientMsgs.get(0),clientMsgs.get(1),clientMsgs.get(2));
+                        dataOutputStream.writeUTF(serverMsg);
+                        dataOutputStream.flush();
+                        break;
+                    case "3":
+                        return "exit";
+                }
+
+            }
+        }catch (IOException e){
             throw new RuntimeException(e);
         }
     }
--- Hangman/src/Server.java
+++ Hangman/src/Server.java
@@ -8,54 +8,41 @@ public class Server {
         String password = "";
         try{
             ServerSocket serverSocket = new ServerSocket(6666);
-
-
             while (true){
                 Socket client = serverSocket.accept();
                 ImpUserServices impUserServices= new ImpUserServices();
-//            new Thread((Runnable) impUserServices).start();
-                System.out.println("New client connected" + client.getInetAddress().getHostAddress());
-
-//                dataOutputStream.writeUTF("Enter Username: ");
-//                username = (String) dataInputStream.readUTF();
-//                dataOutputStream.writeUTF("Enter Password: ");
-//                password = (String) dataInputStream.readUTF();
-//                boolean state =  object.login(username, password);
+                System.out.println("New client connected " + client.getInetAddress().getHostAddress());
                 ClientHandler clientHandler = new ClientHandler(client);
                 new Thread(clientHandler).start();
-
             }
-
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-
-
-    private static class GameServer implements Runnable {
-
-        //dah hy2sm el servers le 3dd el clients
-        //lo single player hyfdl 3la el server bta3o
-        //lo multiplayer hy2dr y2ma y3ml room 3la el server dah, ya hy3ml join le server tany
-        //m4 3arf lessa hal 23ml function bta5od clients tanyeen 2zay in case of multiplayer...
-        //y3ny hal 22dr 25ly function bt3ml accept lel clients el tanyeen? wla el function deh bta5od array?
-        //ana h4oof 2wl wa7da.
-
-//    private int port;
-//    public GameServer(int port) throws IOException {
-//        this.port = port;
+//    private static class GameServer implements Runnable {
+//
+//        //dah hy2sm el servers le 3dd el clients
+//        //lo single player hyfdl 3la el server bta3o
+//        //lo multiplayer hy2dr y2ma y3ml room 3la el server dah, ya hy3ml join le server tany
+//        //m4 3arf lessa hal 23ml function bta5od clients tanyeen 2zay in case of multiplayer...
+//        //y3ny hal 22dr 25ly function bt3ml accept lel clients el tanyeen? wla el function deh bta5od array?
+//        //ana h4oof 2wl wa7da.
+//
+////    private int port;
+////    public GameServer(int port) throws IOException {
+////        this.port = port;
+////    }
+//
+//
+//
+//        public void run()
+//        {
+//
+//        }
+//
+//
 //    }
-
-
-
-        public void run()
-        {
-
-        }
-
-
-    }
 
 }
 
