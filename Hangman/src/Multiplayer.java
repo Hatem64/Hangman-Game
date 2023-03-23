cf0a253e27c5b288adf4f1d30eabbe88acfcf037
Zoldikxx <mziad316@yahoo.com>
2023-03-23 17:47:08 +0200

    Added some functions to Multiplayer Class
    I;ve finished the turnTracker method and multiplayer class is almost finished
    one thing left to be done which is the else block in the startGame method
    it should be implemented based on the mode parameter weather it was 1v1 or 2v2

--- Hangman/src/Multiplayer.java
+++ Hangman/src/Multiplayer.java
@@ -1,9 +1,15 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Multiplayer {
+    int numOfAttempts = 0;
+    int max = 20;
+    int min = 1;
+    int range = max - min + 1;
+    String serverMsg = "";
+    String clientMsg = "";
+    private String user = null;
+    int score = 0;
     // List of all players waiting to join a team
     // I'm not sure about the type of the list ya hatem
     private final List<ImpUserServices> waitingPlayers = new ArrayList<ImpUserServices>();
@@ -53,6 +59,7 @@ public class Multiplayer {
         } else {
             //Start the game
             //To be continued
+            
         }
     }
 
@@ -103,13 +110,8 @@ public class Multiplayer {
                 currentPlayer.sendMessage("Your turn. Enter a letter: ");
                 String guess = currentPlayer.readMessage();
 
-                //guess must be validated somehow
-                //if guess is correct
-                //currentPlayer.sendMessage("Correct guess !");
-//                else{
-//                    numAttempts--;
-//                    currentPlayer.sendMessage("Incorrect guess !");
-//                }
+                //guess must be validated somehow => call the game logic function
+                game(numOfAttempts,"RegisteredUsers.txt");
 
                 if (currentTeam == team1) {
                     currentTeam = team2;
@@ -124,4 +126,119 @@ public class Multiplayer {
             }
         }
     }
+    public void game(int numOfAttempts, String fileName) throws IOException {
+        ClientHandler player = null;
+        this.numOfAttempts = numOfAttempts;
+        int score = 0;
+        int iteration = 0;
+        int rand = (int)(Math.random() * range) + min;
+        BufferedReader bf = new BufferedReader(new FileReader(fileName));
+        Scanner reader;
+        //array contains the number of letters in the word
+        String word = "";
+
+        try {
+            reader = new Scanner(bf);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i != rand; i++){
+            word = reader.nextLine();
+        }
+        int numOfDashes = word.length();
+        String dashes = "";
+        player.sendMessage("3,Be ware The server will only read the first character \nof the string if one is entered. \nso please enter a single character!");
+        while (true){
+            boolean rightLetter = false;
+            if(iteration == 0){
+                for(int i = 0; i<numOfDashes; i++){
+                    dashes = dashes.concat("_");
+                }
+            }else {
+                for(int i = 0; i<numOfDashes; i++){
+                    clientMsg = clientMsg.toLowerCase();
+                    if(word.charAt(i) == clientMsg.toLowerCase().charAt(0)){
+                        StringBuilder stringBuilder = new StringBuilder(dashes);
+                        stringBuilder.setCharAt(i, clientMsg.charAt(0));
+                        dashes = stringBuilder.toString();
+                        score++;
+                        rightLetter = true;
+                    }
+                }
+            }
+            if (numOfAttempts == 0){
+                player.sendMessage("3,You have failed! Shame on you... \nThe word was: "+ word +"\nBetter luck next time...." );
+                String num = Integer.toString(score);
+                addScoreToUser(num);
+                break;
+            }
+            else if (score == word.length()){
+                player.sendMessage("3,Well Done!!!\nScore: "+ score);
+                String num = Integer.toString(score);
+                addScoreToUser(num);
+                break;
+            }
+            if (rightLetter == false){
+                numOfAttempts--;
+            }
+            player.sendMessage("3,\nScore: "+ score +"\nNumber of attempts: "+ numOfAttempts +"\nThe word: ");
+            player.sendMessage("1" + dashes);
+            player.readMessage();
+            iteration++;
+        }
+    }
+
+    public void addScoreToUser(String Score){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("Score.txt", false));)
+        {
+            int lineNum = 0;
+            BufferedReader bf = new BufferedReader(new FileReader("RegisteredUsers.txt"));
+            Scanner reader;
+            try {
+                reader = new Scanner(bf);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            while (reader.hasNext()) {
+                String line = reader.nextLine();
+                if (line.contains(user)) {
+                    bw.write(line + "," + score);
+                    bw.newLine();
+                    user = line + "," + score;
+                }else{
+                    bw.write(line);
+                    bw.newLine();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try( BufferedWriter bw = new BufferedWriter(new FileWriter("RegisteredUsers.txt", false));)
+        {
+            int lineNum = 0;
+            BufferedReader bf = new BufferedReader(new FileReader("Score.txt"));
+            Scanner reader;
+            try {
+                reader = new Scanner(bf);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            while (reader.hasNext()) {
+                String line = reader.nextLine();
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
